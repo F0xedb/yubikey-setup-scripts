a732d8126c5bdcb91006444417568a7a469552fe
F0xedb <tom@odex.be>
2022-09-21 16:36:28 +0200

    BUG: Only allow a specific serial id to login
    Previously we only relied on vendor & Product ID, which is really
    dangerous.
    
    Meaning people can still unlock devices when having the same hardware.
    
    This patch takes the serial id of the key in mind as well to lock the
    device

--- install.sh
+++ install.sh
@@ -5,8 +5,24 @@ if [[ "$(id -u)" != "0" ]]; then
 	exit 1
 fi
 
+if [[ ! -d "/home/$1"  || "$1" == "" ]]; then
+	echo "Please specify the user to run as"
+	echo "bash $0 $USER"
+	exit 1
+fi	
+
 cp ./scripts/* /usr/local/bin/
 
 cp rules/* /etc/udev/rules.d/
 
 udevadm control --reload-rules
+
+
+SERIAL=$(su $1 -c 'gpg --card-status' | grep Serial | awk '{print $NF}')
+
+echo "Found yubikey serial number: $SERIAL"
+
+if [[ "$SERIAL" -gt "0" ]]; then
+	echo "export YK_SERIAL='$SERIAL'" >> ~/.profile
+	echo "export YK_SERIAL='$SERIAL'" | sudo tee -a /root/.profile
+fi
